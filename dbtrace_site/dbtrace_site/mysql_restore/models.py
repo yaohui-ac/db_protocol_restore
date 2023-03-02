d44f085db329c6f5d9531dd38a145ae5d53e0264
yaohui_sama <779073207@qq.com>
2023-03-03 00:06:10 +0800

    ADD TEST CODE

--- dbtrace_site/dbtrace_site/mysql_restore/models.py
+++ dbtrace_site/dbtrace_site/mysql_restore/models.py
@@ -3,23 +3,6 @@ from inits import mysql_restore_info
 # collection_name mongodb
 
 
-def __insert_some_cols():
-    cols = []
-    for i in range(100):
-        cols.append({
-
-            "date": 1677511969,
-            "source_ip": "114.114.114.114",
-            "sql_text": "SELECT 114514 FROM hhhaaa",
-            "target_ip": "1.1.1.1",
-            "user_name": "test" + i,
-            "source_port": 1111,
-            "target_port": 2222,
-        })
-    x = mysql_restore_info.insert_many(cols)
-    print("insert: ", x.inserted_ids)
-
-
 def find_cols(collection_name, page_no, page_size, is_desc=True):
 
     # query_dict 指定要查询的项
