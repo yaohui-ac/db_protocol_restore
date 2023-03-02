cf14022365c1f5edac167a8e8aeca86753dc11c9
yaohui_sama <779073207@qq.com>
2023-03-02 23:15:34 +0800

    ADD sql query

--- dbtrace_site/dbtrace_site/mysql_restore/models.py
+++ dbtrace_site/dbtrace_site/mysql_restore/models.py
@@ -1,11 +1,39 @@
 from .inits import mysql_restore_info
 
+# collection_name mongodb
 
-def find_collections(page_no, page_size, is_desc=True):
+
+def find_cols(collection_name, page_no, page_size, is_desc=True):
+
     # query_dict 指定要查询的项
     query_dict = {"date": 1, "source_ip": 1, "sql_text": 1, "user_name": 1}
 
-    cols = mysql_restore_info.find({}, query_dict) \
+    cols = collection_name.find({}, query_dict) \
+        .sort("date", -1)\
+        .limit(limit=page_size)\
+        .skip((page_no - 1) * page_size)
+    return cols
+
+
+def find_cols_by_user(collection_name, user_name, page_no, page_size, is_desc=True):
+    find_condition = {
+        "user_name": {"$regex": user_name},
+    }
+    query_dict = {"date": 1, "source_ip": 1, "sql_text": 1, "user_name": 1}
+    cols = collection_name.find(find_condition, query_dict) \
+        .sort("date", -1)\
+        .limit(limit=page_size)\
+        .skip((page_no - 1) * page_size)
+    return cols
+
+
+def find_cols_by_timerange(collection_name, begin_timestamp, end_timestamp,
+                           page_no, page_size, is_desc=True):
+    find_condition = {
+        "date": {"$gte": begin_timestamp, "$lte": end_timestamp},
+    }
+    query_dict = {"date": 1, "source_ip": 1, "sql_text": 1, "user_name": 1}
+    cols = mysql_restore_info.find(find_condition, query_dict) \
         .sort("date", -1)\
         .limit(limit=page_size)\
         .skip((page_no - 1) * page_size)
---
+++ start_project.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# 启动mongoDB
+mongod --dbpath /var/lib/mongo --logpath /var/log/mongodb/mongod.log --fork
