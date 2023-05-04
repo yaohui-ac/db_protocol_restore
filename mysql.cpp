dd84be10978e9def3cea4a8c5862a419023f6ac9
duanfaxin <2736913400@qq.com>
2023-05-04 16:48:24 +0800

    fix

--- mysql.cpp
+++ mysql.cpp
@@ -78,3 +78,64 @@ int main(int argc, char **argv)
 
   return EXIT_SUCCESS;
 }
+
+
+
+// #include <iostream>
+// #include <nids.h>
+
+// void handle_mysql_request(struct tcp_stream *tcp, void **arg)
+// {
+//     char buffer[1024];
+//     struct half_stream *hlf;
+
+//     if (tcp->nids_state == NIDS_JUST_EST)
+//     {
+//         std::cout << "New TCP connection: " << inet_ntoa(*(struct in_addr *) &tcp->addr.saddr) << ":"
+//                   << ntohs(tcp->addr.source) << " -> " << inet_ntoa(*(struct in_addr *) &tcp->addr.daddr) << ":"
+//                   << ntohs(tcp->addr.dest) << std::endl;
+//     }
+//     else if (tcp->nids_state == NIDS_DATA)
+//     {
+//         hlf = &tcp->server;
+
+//         // Check if the stream contains a MySQL protocol packet
+//         if (*(unsigned char *) hlf->data == 0x10 || *(unsigned char *) hlf->data == 0x1a)
+//         {
+//             // Extract the MySQL query string
+//             sscanf(hlf->data, "%*c%*[^\0]%s", buffer);
+//             std::cout << "MySQL query from " << inet_ntoa(*(struct in_addr *) &tcp->addr.saddr) << ":"
+//                       << ntohs(tcp->addr.source) << " to " << inet_ntoa(*(struct in_addr *) &tcp->addr.daddr) << ":"
+//                       << ntohs(tcp->addr.dest) << ":\n" << buffer << std::endl;
+//         }
+//     }
+// }
+
+// int main(int argc, char **argv)
+// {
+//     if (argc != 2)
+//     {
+//         std::cerr << "Usage: " << argv[0] << " <interface>" << std::endl;
+//         exit(EXIT_FAILURE);
+//     }
+
+//     if (!nids_init())
+//     {
+//         fprintf(stderr, "%s\n", nids_errbuf);
+//         exit(EXIT_FAILURE);
+//     }
+
+//     // Set BPF filter to capture only MySQL protocol traffic
+//     nids_params.pcap_filter = "tcp port 3306";
+
+//     // Register a callback function for MySQL protocol
+//     nids_register_tcp((void *) handle_mysql_request);
+
+//     if (nids_run() == 0)
+//     {
+//         fprintf(stderr, "Failed to run NIDS: %s\n", nids_errbuf);
+//         exit(EXIT_FAILURE);
+//     }
+
+//     return EXIT_SUCCESS;
+// }
